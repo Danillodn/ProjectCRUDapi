70856320de0b0758305aa84d4628304dde5953c0
Danillodn <danillodiasnascimento@gmail.com>
2023-05-12 16:23:38 -0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react" 
+import React from "react"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Edit from "./pages/edit"
+import Home from "./pages/home"
+import Post from "./pages/post"
+import Readmore from "./pages/readmore"
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>teste</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/readmore" element={<Readmore />} />
+      </Routes>
+    </Router>
   );
 }
 

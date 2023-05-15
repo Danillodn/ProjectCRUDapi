b25791e7f8bcad08749ecf3d4ca125441b93beda
Danillodn <danillodiasnascimento@gmail.com>
2023-05-15 13:21:51 -0300

    added card read more

--- src/pages/Readmore/readmore.js
+++ src/pages/Readmore/readmore.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 
+
+
 const Readmore = () => {
   return (
     <div>
       <Header/>
+
+      <main>
+        <div className='cards'>
+          <div className='card'>
+            <header>
+              <h2>Consumindo API</h2>
+            </header>
+            <div className='line'></div>
+            <p>Aqui eu vou falar sobre o conteudo, Aqui eu vou falar sobre o conteudo, Aqui eu vou falar sobre o conteudo</p>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }

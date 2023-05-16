7dcd96598dad58406958fa77dd7155ea512be3d2
Danillodn <danillodiasnascimento@gmail.com>
2023-05-16 15:42:14 -0300

    added title and description with axios

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,38 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 import HeaderMain from '../../components/HeaderMain/HeaderMain'
-
 import More from '../../images/more.svg'
-
 import '../Home/home.css'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    axios.get("https://dull-dog-gloves.cyclic.app/list_posts")
+      .then((response) => {
+        setPosts(response.data.posts)
+      })
+      .catch(() => {
+        console.log("ERROR!!!")
+      })
+  }, [])
+
   return (
     <div>
       <HeaderMain />
-
       <main>
         <div className='cards'>
-          <div className='card'>
-            <header>
-              <h2>Consumindo API</h2>
-              <img src={More} alt=''/>
-            </header>
-            <div className='line'></div>
-            <p>Aqui eu vou falar sobre o conteudo</p>
-            <div className='btns'>
-              <div className='btn-edit'>
-                <Link to="/edit"><button>Edit</button></Link>
+          {posts.map((post, key) => {
+            return (
+              <div className='card' key={key}>
+                <header>
+                  <h2>{post.title}</h2>
+                  <img src={More} alt='' />
+                </header>
+                <div className='line'></div>
+                <p>{post.description}</p>
+                <div className='btns'>
+                  <div className='btn-edit'>
+                    <Link to="/edit"><button>Edit</button></Link>
+                  </div>
+                  <div className='btn-readmore'>
+                    <Link to="/readmore"><button>Read More</button></Link>
+                  </div>
+                  <div className='btn-delete'>
+                    <button>Delete</button>
+                  </div>
+                </div>
               </div>
-              <div className='btn-readmore'>
-                <Link to="/readmore"><button>Read More</button></Link>
-              </div>
-              <div className='btn-delete'>
-                <button>Delete</button>
-              </div>
-            </div>
-          </div>
+            )
+          })}
         </div>
       </main>
     </div>

d448562beeed0f60f57cf989852ae9576ec9c65d
Danillodn <danillodiasnascimento@gmail.com>
2023-06-26 16:53:53 -0300

    edited rote

--- src/redux/HomePosts/homeAPI.js
+++ src/redux/HomePosts/homeAPI.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const fetchListPosts = () => {
-    return axios.get("https://dull-dog-gloves.cyclic.app/list_posts");
+    return axios.get("https://dark-sunbonnet-mite.cyclic.app/list_posts");
 };
 
 export const fetchDeletePosts = (id) => {
-    return axios.delete(`https://dull-dog-gloves.cyclic.app/delete_post/${id}`);
+    return axios.delete(`https://dark-sunbonnet-mite.cyclic.app/delete_post/${id}`);
 };

9be8ee52b20b3bf22d2dffa58c928b1d4689d384
Danillodn <danillodiasnascimento@gmail.com>
2023-06-26 16:53:17 -0300

    added redux

--- src/pages/Readmore/readmore.js
+++ src/pages/Readmore/readmore.js
@@ -1,44 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import Header from '../../components/Header/Header'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-
+import React, { useEffect } from 'react';
+import Header from '../../components/Header/Header';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { showPosts } from '../../redux/ReadPosts/readSlice';
 
 
 const Readmore = () => {
 
-
-  const [readmore, setReadmore] = useState({})
-
-  const { id } = useParams()
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts.posts);
 
   useEffect(() => {
-    axios.get(`https://dull-dog-gloves.cyclic.app/show_post/${id}`)
-      .then((response) => {
-        setReadmore(response.data.post)
-      })
-      .catch(() => {
-        console.log("ERROR")
-      })
-  },)
-
+    dispatch(showPosts(id));
+  }, [id, dispatch]);
 
   return (
     <div>
       <Header />
       <main>
         <div className='cards'>
-          <div className='card'>
-            <header>
-              <h2>{readmore.title}</h2>
-            </header>
-            <div className='line'></div>
-            <p>{readmore.content}</p>
-          </div>
+          {posts.map((posts, key) => {
+            return (
+              <div className='card' key={key}>
+                <header>
+                  <h2>{posts.title}</h2>
+                </header>
+                <div className='line'></div>
+                <p>{posts.content}</p>
+              </div>
+            );
+          })};
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Readmore
+export default Readmore;

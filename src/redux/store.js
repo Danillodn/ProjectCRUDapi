462e109464072a3d325073defcfe32ac204406ea
Danillodn <danillodiasnascimento@gmail.com>
2023-06-27 15:37:28 -0300

    added read and add in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './HomePosts/homeSlice';
 import readReducer from './ReadPosts/readSlice';
-
+import addReducer from './AddPosts/addSlice'
 
 const store = configureStore({
     reducer: {
       posts: postsReducer,
       read: readReducer,
+      add: addReducer,
     }
   });
 

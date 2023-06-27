b09c62343f3bf6a37b0789d95027de1c233fdc60
Danillodn <danillodiasnascimento@gmail.com>
2023-06-27 18:37:49 -0300

    added editReducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from './HomePosts/homeSlice';
 import readReducer from './ReadPosts/readSlice';
-import addReducer from './AddPosts/addSlice'
+import addReducer from './AddPosts/addSlice';
+import editReducer from './EditPosts/editSlice';
 
 const store = configureStore({
     reducer: {
       posts: postsReducer,
       read: readReducer,
       add: addReducer,
+      edit: editReducer,
     }
   });
 
